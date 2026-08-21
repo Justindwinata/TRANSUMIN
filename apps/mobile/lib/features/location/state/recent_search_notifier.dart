fd8f333f6dd86ba51642bc621f5c7e36d8f0b09a
Justin Dwinata <41523010170@student.mercubuana.ac.id>
2026-08-21 16:19:04 +0700

    feat: recent search state with deduplication

---
+++ apps/mobile/lib/features/location/state/recent_search_notifier.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../domain/models.dart';
+
+class RecentSearchNotifier extends StateNotifier<List<Place>> {
+  static const int maxRecent = 10;
+  
+  RecentSearchNotifier() : super([]);
+
+  void addRecent(Place place) {
+    state = [
+      place,
+      ...state.where((p) => p.id != place.id || p.name != place.name).take(maxRecent - 1),
+    ];
+  }
+
+  void clearRecent() {
+    state = [];
+  }
+}
+
+final recentSearchProvider = StateNotifierProvider<RecentSearchNotifier, List<Place>>((ref) {
+  return RecentSearchNotifier();
+});
