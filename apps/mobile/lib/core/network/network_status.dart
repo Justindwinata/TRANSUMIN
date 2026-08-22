b531d31f3501551245d0409601e253b6d69b8e28
Justin Dwinata <41523010170@student.mercubuana.ac.id>
2026-08-22 11:05:51 +0700

    refactor(network): add network status provider for UI resilience

---
+++ apps/mobile/lib/core/network/network_status.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class NetworkStatus {
+  final bool isConnected;
+  final String? errorMessage;
+
+  const NetworkStatus({this.isConnected = true, this.errorMessage});
+}
+
+class NetworkStatusNotifier extends StateNotifier<NetworkStatus> {
+  NetworkStatusNotifier() : super(const NetworkStatus());
+
+  void setConnected(bool connected, {String? message}) {
+    state = NetworkStatus(isConnected: connected, errorMessage: message);
+  }
+}
+
+final networkStatusProvider = StateNotifierProvider<NetworkStatusNotifier, NetworkStatus>((ref) {
+  return NetworkStatusNotifier();
+});
